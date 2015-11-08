d6ae8979d3f69bbabc1f59cef1a734c738d886e1
amstut_a <arthur.amstutz@gmail.com>
2015-11-08 21:20:48 +0100

    add layout

--- onload.js
+++ onload.js
@@ -1,17 +1,24 @@
 function load() {
     try {
-	var oP = document.body.getElementsByTagName('ul')[0];
+	var oP = document.body.getElementsByTagName('section')[0];
 	document.body.removeChild(oP);
     }
-    catch (e) {}
+    catch (e) {
+    }
 
+    var aside = document.createElement('section');
+    var title = document.createElement('h2');
     var lst = document.createElement('ul');
+
+    title.innerHTML = "My series";
+    aside.appendChild(title);
     for (var key in localStorage) {
 	var elem = document.createElement('li');
 	elem.innerHTML = key + ' : ' + localStorage[key];
 	lst.appendChild(elem);
     }
-    document.body.insertBefore(lst, document.body.childNodes[2]);
+    aside.appendChild(lst);
+    document.body.insertBefore(aside, document.body.childNodes[4]);
 }
 
 window.onload = function() {
